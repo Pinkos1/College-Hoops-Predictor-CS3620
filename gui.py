7a520c1da1733e52f96fdd4f6b37a1180ef6c1c4
Pinkos1 <Pinkos2003@gmail.com>
2025-11-18 20:35:23 -0500

    added intro documentation and placeholder teams

---
+++ gui.py
@@ -0,0 +1,17 @@
+
+"""
+@Author - Adam Pinkos
+@File - gui.py
+@Date - 11/18/2025
+@Brief - Simple gui that you can select two colleges and receive a predicted score outcome. Even if the college never play each other
+
+"""
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+# Picked random colleges as placeholers before I insert data into the program 
+TEAMS = [
+    "Alabama", "Akron", "Gonzaga", "Duke",
+    "Kansas", "Kentucky", "Purdue", "UConn"
+]
